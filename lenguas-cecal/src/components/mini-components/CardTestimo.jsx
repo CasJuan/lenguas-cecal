20bf74a35b9d02c260cd3e66431550c5a305d32a
JuanCas <jcastro@colegium.com>
2026-04-17 16:47:41 -0300

    testimonios mas card testi

--- lenguas-cecal/src/components/mini-components/CardTestimo.jsx
+++ lenguas-cecal/src/components/mini-components/CardTestimo.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Quote, Star } from 'lucide-react'
 
-export const CardTestimo = ({desc,nombre,profesion,curso}) => {
+export const CardTestimo = ({ desc, nombre, profesion, curso }) => {
     return (
-        <div className='flex flex-col items-start justify-center'>
+        <div className='flex flex-col items-start justify-center gap-5 h-80 w-80 bg-hover rounded-2xl shadow-2xl'>
             <Quote />
-            <Star />
-            <p>{desc}</p>
-            <div></div>
-            <div>
-            <div>
-                <h5>{nombre}</h5>
-                <p>{profesion}</p>
+            <div className='flex flex-row items-center justify-center gap-2'>
+                <Star size={15} />
+                <Star size={15} />
+                <Star size={15} />
+                <Star size={15} />
+                <Star size={15} />
             </div>
-            <p>{curso}</p>
+            <p className='text-base text-tertiary'>{desc}</p>
+            <div></div>
+            <div className='flex flex-row items-center justify-center'>
+                <div className='flex flex-col items-start justify-center'>
+                    <h5>{nombre}</h5>
+                    <p>{profesion}</p>
+                </div>
+                <p className='bg-hover rounded-2xl text-base text-tertiary'>{curso}</p>
             </div>
         </div>
     )
